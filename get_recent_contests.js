d09cca63f3a55cce1f6ebd6e1c99e38583e005f3
Lina Abdelkhalig <lelsammani0@gmail.com>
2025-11-23 14:02:36 +0200

    initial commit

---
+++ get_recent_contests.js
@@ -0,0 +1,39 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+const BASE_URL = __ENV.LUQTA_BASE_URL;
+const API_GROUP = __ENV.LUQTA_API_GROUP;
+const AUTH_TOKEN = __ENV.LUQTA_TOKEN || "";
+
+const paging = JSON.stringify({ page: 1, per_page: 20 });
+
+const URL_CONTEST_LIST = `${BASE_URL}/${API_GROUP}/getrecentcontest?paging=${encodeURIComponent(paging)}`; //more parameters here
+
+export const options = {
+  vus: 100,
+  duration: "3m",
+  thresholds: {
+    http_req_failed: ["rate<0.01"],
+    http_req_duration: ["p(95)<1500"],
+  },
+};
+
+function authHeaders() {
+  const headers = { "Content-Type": "application/json" };
+  if (AUTH_TOKEN) {
+    headers["Authorization"] = `Bearer ${AUTH_TOKEN}`;
+  }
+  return headers;
+}
+
+export default function () {
+  const res = http.get(URL_CONTEST_LIST, {
+    headers: authHeaders(),
+  });
+
+  check(res, {
+    "status is 200": (r) => r.status == 200,
+  });
+
+  sleep(1); //to simulate user scrolling a bit before the next request
+}
