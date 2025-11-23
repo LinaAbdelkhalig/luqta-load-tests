6cba0cefdb9f068f8dd089aca071b78723f25231
Lina Abdelkhalig <lelsammani0@gmail.com>
2025-11-23 17:01:10 +0200

    changes to the env files

--- get_recent_contests.js
+++ get_recent_contests.js
@@ -10,8 +10,8 @@ const paging = JSON.stringify({ page: 1, per_page: 20 });
 const URL_CONTEST_LIST = `${BASE_URL}/${API_GROUP}/getrecentcontest?paging=${encodeURIComponent(paging)}`; //more parameters here
 
 export const options = {
-  vus: 100,
-  duration: "3m",
+  vus: 1,
+  duration: "5s",
   thresholds: {
     http_req_failed: ["rate<0.01"],
     http_req_duration: ["p(95)<1500"],
@@ -34,6 +34,8 @@ export default function () {
   check(res, {
     "status is 200": (r) => r.status == 200,
   });
+
+  //console.log("STATUS:", res.status, "BODY:", res.body); // to output the error
 
   sleep(1); //to simulate user scrolling a bit before the next request
 }
